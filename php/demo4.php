30d45687d5685da5f3636fdfbaa7d1c80eb71d32
allenessy <allenessy@gmail.com>
2013-06-08 04:18:43 +0800

    Update demo4.php

--- php/demo4.php
+++ php/demo4.php
@@ -1,4 +1,10 @@
 <?php
+//数组排序
+echo '<table border="0" cellspacing="3" cellpadding="3" align="center">';
+echo '<tr>
+<td><h2>Rating</h2></td>
+<td><h2>Title</h2></td>
+</tr>';
 $movies=array(
 	'Casablanca'=>10,
 	'To kill a Mockingbird'=>10,
@@ -10,20 +16,23 @@ $movies=array(
 
 echo '<tr><td colspan="2"><b>In their original order:</b></td></tr><br>';
 foreach ($movies as $title=>$rating){
-	echo "<ul><li>$rating</li>
-	<li>$title</li></ul>";
+	echo "<tr><td>$rating</td>
+	<td>$title</td></tr>";
 }
 
+//按评级降序以数字方式对数组排序
 ksort($movies);
 echo '<tr><td colspan="2"><b>Sorted by title:</b></td></tr><br>';
 foreach ($movies as $title=>$rating){
-	echo "<ul><li>$rating</li>
-	<li>$title</li></ul>";
+	echo "<tr><td>$rating</td>
+	<td>$title</td></tr>";
 }
 
+//按名称以字母顺序对数组排序
 arsort($movies);
 echo '<tr><td colspan="2"><b>Sorted by rating:</b></td></tr><br>';
 foreach ($movies as $title=>$rating){
-	echo "<ul><li>$rating</li>
-	<li>$title</li></ul>";
+	echo "<tr><td>$rating</td>
+	<td>$title</td></tr>";
 }
+echo '</table>';
