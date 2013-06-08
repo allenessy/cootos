7301c991f3b6678dc927defc435ec0196bcb43d6
allenessy <allenessy@gmail.com>
2013-06-09 01:25:44 +0800

    Create header.php

---
+++ php/header.php
@@ -0,0 +1,9 @@
+<?php 
+echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
+echo '<html xmlns="http://www.w3.org/1999/xhtml" lang="zh-CN">';
+echo '<head profile="http://gmpg.org/xfn/11">';
+echo '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />';
+echo "<title>$page_title</title>";
+echo '<link rel="stylesheet" href="./css/bootstrap.css" type="text/css" media="screen" />';
+echo '<script type="text/javascript" src="./js/jquery-2.0.1.js"></script>';
+echo '<body>';
