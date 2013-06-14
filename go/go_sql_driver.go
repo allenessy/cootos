06438e4d186655b060539f7aab95a64d6e01f7d2
allenessy <allenessy@gmail.com>
2013-06-15 03:05:27 +0800

    Create go_sql_driver.go

---
+++ go/go_sql_driver.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+  "database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db,err := sql.Open("mysql","ciswp:xt246402@tcp(localhost:3306)/wp?charset=utf8")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+/*	rows,err := db.Query("SELECT * FROM user")
+	if err!= nil{
+		panic(err.Error())
+	}	
+	columns,err := rows.Columns()
+	if err !=nil{
+		panic(err.Error())
+	}*/
+}
