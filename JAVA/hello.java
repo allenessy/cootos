1e5137321ee753d391f997688da278793f562662
allenessy <allenessy@gmail.com>
2013-06-29 04:17:48 +0800

    Update hello.java

--- JAVA/hello.java
+++ JAVA/hello.java
@@ -1,9 +1,10 @@
 public class hello {
 
-  /**
+	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		System.out.println("Hello world!");
 	}
 
