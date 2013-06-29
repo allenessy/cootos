85a1c121a39c24e7bc5f38a4030bd23201e4c69a
allenessy <allenessy@gmail.com>
2013-06-29 21:15:22 +0800

    Update test4.go

--- go/test4.go
+++ go/test4.go
@@ -15,18 +15,18 @@ import (
 */
 
 func main() {
-    fmt.Println(^2)
-  	fmt.Println(1 ^ 2)
-  	fmt.Println(!true)
-  	fmt.Println(1 << 10)
-	  fmt.Println(1 << 10 << 10)
-  	fmt.Println(1 << 10 << 10 >> 10)
-  	fmt.Println(6 & 11)
-	  fmt.Println(6 | 11)
-  	fmt.Println(6 ^ 11)
-	  fmt.Println(6 &^ 11)
-  	a := 1
-  	if a > 0 && (10/a) > 1 {
-	  	fmt.Println("OK")
+	fmt.Println(^2)
+ 	fmt.Println(1 ^ 2)
+	fmt.Println(!true)
+	fmt.Println(1 << 10)
+	fmt.Println(1 << 10 << 10)
+	fmt.Println(1 << 10 << 10 >> 10)
+	fmt.Println(6 & 11)
+	fmt.Println(6 | 11)
+	fmt.Println(6 ^ 11)
+	fmt.Println(6 &^ 11)
+	a := 1
+	if a > 0 && (10/a) > 1 {
+	fmt.Println("OK")
   	}
 }
