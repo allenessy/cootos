d0671861bc290c53e5aee52fa6581d16dc20f85c
allenessy <allenessy@gmail.com>
2013-06-29 21:13:38 +0800

    Create test4.go

---
+++ go/test4.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+   "fmt"
+)
+
+/*
+  6: 0110
+ 11: 1011
+ ----------
+ &   0010 = 2
+ |   1111 = 15
+ ^   1101 = 13
+ &^  0100 = 4
+*/
+
+func main() {
+    fmt.Println(^2)
+  	fmt.Println(1 ^ 2)
+  	fmt.Println(!true)
+  	fmt.Println(1 << 10)
+	  fmt.Println(1 << 10 << 10)
+  	fmt.Println(1 << 10 << 10 >> 10)
+  	fmt.Println(6 & 11)
+	  fmt.Println(6 | 11)
+  	fmt.Println(6 ^ 11)
+	  fmt.Println(6 &^ 11)
+  	a := 1
+  	if a > 0 && (10/a) > 1 {
+	  	fmt.Println("OK")
+  	}
+}
