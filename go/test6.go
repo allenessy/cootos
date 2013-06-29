00581cac720899884b40a093ea544d076307df91
allenessy <allenessy@gmail.com>
2013-06-29 22:13:55 +0800

    Create test6.go

---
+++ go/test6.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+  "crypto/md5"
+	"crypto/sha1"
+	"fmt"
+	"io"
+)
+
+//对字符串进行MD5哈希
+func a(data string) string {
+	t := md5.New()
+	io.WriteString(t, data)
+	return fmt.Sprintf("%x", t.Sum(nil))
+}
+
+//对字符串进行SHA1哈希
+func b(data string) string {
+	t := sha1.New()
+	io.WriteString(t, data)
+	return fmt.Sprintf("%x", t.Sum(nil))
+}
+
+func main() {
+	var data string = "abc"
+	fmt.Printf("MD5:%s\n", a(data))
+	fmt.Printf("SHA1:%s\n", b(data))
+}
