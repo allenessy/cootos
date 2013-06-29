6d13aa67c0a44b5878fb9661374503a372aa8ea0
allenessy <allenessy@gmail.com>
2013-06-29 21:37:46 +0800

    Create test5.go

---
+++ go/test5.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  var a int = 65
+	b := string(a)
+	fmt.Println(b)
+	c := "hello world"
+	length := len(c)
+	fmt.Println(length)
+	d := 1
+	var p *int = &d
+	fmt.Println(p)
+	fmt.Println(*p)
+}
