d34c73be7c5880571b93837d6f9fd406a190de7b
allenessy <allenessy@gmail.com>
2013-06-29 19:24:45 +0800

    Create test3.go

---
+++ go/test3.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+  "strconv"
+)
+
+func main() {
+	i, err := strconv.Atoi("12345")
+	if err != nil {
+		panic(err)
+	}
+	i += 3
+	println(i)
+
+	s := strconv.Itoa(12345)
+	s += "3"
+	println(s)
+}
